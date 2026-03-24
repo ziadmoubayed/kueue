f790ebf4eb7f402e7638e9ba78dc51d65c5554e3
ziad <ziadmoubayed@gmail.com>
2026-03-24 08:09:04 +0100

    fix(kueueviz): revert oidc import

--- cmd/kueueviz/backend/kubernetes_client.go
+++ cmd/kueueviz/backend/kubernetes_client.go
@@ -23,7 +23,6 @@ import (
 	"os"
 	"path/filepath"
 
-	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	corev1 "k8s.io/api/core/v1"
 	schedulingv1 "k8s.io/api/scheduling/v1"
 	"k8s.io/apimachinery/pkg/runtime"
